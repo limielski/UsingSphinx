181a76a4c1fa4b566b27eeb4fa972534b5eab76c
Leszek Imielski <LeszekImielski@gmail.com>
2023-10-16 18:01:38 +0200

    zmiana w yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ language = 'pl'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
